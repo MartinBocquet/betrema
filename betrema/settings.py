3b7673b34c8844b5788b55bc07e854a00fd3ac05
mbocquet <martin.bocquet@gmail.com>
2018-06-11 22:38:18 +0200

    premier test en ligne

--- betrema/settings.py
+++ betrema/settings.py
@@ -196,7 +196,7 @@ WSGI_APPLICATION = 'betrema.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+ 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
