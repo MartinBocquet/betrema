da0d980e9f260013eed8fe73fde8fc5a77d87242
mbocquet <martin.bocquet@gmail.com>
2018-06-11 22:50:45 +0200

    premier test en ligne

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'date', views.date_actuelle),
     url(r'pari/(?P<phase>[A-Za-z0-9]*)', views.pari, name = 'pari'),
-    url(r'^inscription/$', views.inscription, name='inscription'),
-    url(r'^deconnect_gen$', auth_views.logout_then_login, name='deconnect_gen'),
-    url(r'^connexion$', auth_views.login, {'template_name': 'app/connexion.html', 'redirect_field_name': '/app/pari'}),
+    url(r'inscription/$', views.inscription, name='inscription'),
+    url(r'deconnect_gen$', auth_views.logout_then_login, name='deconnect_gen'),
+    url(r'connexion$', auth_views.login, {'template_name': 'app/connexion.html', 'redirect_field_name': '/app/pari'}),
     ]
